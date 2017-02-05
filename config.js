9a897d5822718e74cb03b40c7b2c9e2044f46685
Camillo <cangelozzi@gmail.com>
2017-02-05 19:39:30 +0100

    second commit

--- config.js
+++ config.js
@@ -1,6 +1,12 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+  global.DATABASE_URL ||
+  (process.env.NODE_ENV === 'production' ?
+    'mongodb://<dbuser>:<dbpassword>@ds117829.mlab.com:17829/camillo_thinkful'
+  );
 exports.PORT = process.env.PORT || 8080;
+
+
+/*                        
+'mongodb://localhost/shopping-list' :
+'mongodb://localhost/shopping-list-dev'
+*/
